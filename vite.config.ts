c4e0b768fa8fc09c8acf4317b71ef0119bb63225
Daniel <negativ_2002@mail.ru>
2023-09-14 22:22:47 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react-swc";
